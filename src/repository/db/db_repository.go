eb3df1ce93e17f49e40d84e7d8832a5fbc2c4c4e
YePack <vladyslav.martynenko@yahoo.com>
2021-01-18 19:29:58 +0200

    18.01.2021 - 19:29 Oauth DB before integrated Cassandra DB

---
+++ src/repository/db/db_repository.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"github.com/yepack/testOauth-api/src/domain/access_token"
+	"github.com/yepack/testOauth-api/src/utils/errors"
+)
+
+func NewRepository() DbRepository {
+	return &dbRepository{}
+}
+
+type DbRepository interface {
+	GetById(string) (*access_token.AccessToken, *errors.RestErr)
+}
+
+type dbRepository struct {
+}
+
+func (r *dbRepository) GetById(string) (*access_token.AccessToken, *errors.RestErr) {
+	return nil, errors.NewInternalServerError("database connection not implemented yet")
+}
