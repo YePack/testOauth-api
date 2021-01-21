d2232bd9cd8a5f4de88432ece597679f0d5b0393
YePack <vladyslav.martynenko@yahoo.com>
2021-01-21 15:50:49 +0200

    "Oauth lh:8080 + Users lh:8081" Build Has an error with configure public/not user

--- src/app/application.go
+++ src/app/application.go
@@ -2,9 +2,10 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/yepack/testOauth-api/src/domain/access_token"
 	"github.com/yepack/testOauth-api/src/http"
 	"github.com/yepack/testOauth-api/src/repository/db"
+	"github.com/yepack/testOauth-api/src/repository/rest"
+	"github.com/yepack/testOauth-api/src/services/access_token"
 )
 
 var (
@@ -13,9 +14,12 @@ var (
 
 func StartApplication() {
 
-	atHandler := http.NewAccessTokenHandler(access_token.NewService(db.NewRepository()))
+	atHandler := http.NewAccessTokenHandler(
+		access_token.NewService(rest.NewRestUsersRepository(), db.NewRepository()))
+
 	router.POST("/oauth/access_token", atHandler.Create)
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+
 	router.Run("127.0.0.1:8080")
 
 }
---
+++ src/services/access_token/access_token_service.go
@@ -0,0 +1,65 @@
+package access_token
+
+import (
+	"github.com/yepack/testOauth-api/src/domain/access_token"
+	"github.com/yepack/testOauth-api/src/repository/db"
+	"github.com/yepack/testOauth-api/src/repository/rest"
+	"github.com/yepack/testOauth-api/src/utils/errors"
+	"strings"
+)
+
+type Service interface {
+	GetById(string) (*access_token.AccessToken, *errors.RestErr)
+	Create(access_token.AccessTokenRequest) (*access_token.AccessToken, *errors.RestErr)
+	UpdateExpirationTime(access_token.AccessToken) *errors.RestErr
+}
+
+type service struct {
+	restUsersRepo rest.RestUsersRepository
+	dbRepo        db.DbRepository
+}
+
+func NewService(usersRepo rest.RestUsersRepository, dbRepo db.DbRepository) Service {
+	return &service{
+		restUsersRepo: usersRepo,
+		dbRepo:        dbRepo,
+	}
+}
+
+func (s *service) GetById(accessTokenId string) (*access_token.AccessToken, *errors.RestErr) {
+	accessTokenId = strings.TrimSpace(accessTokenId)
+	if len(accessTokenId) == 0 {
+		return nil, errors.NewBadRequestError("invalid access token id")
+	}
+
+	accessToken, err := s.dbRepo.GetById(accessTokenId)
+	if err != nil {
+		return nil, err
+	}
+	return accessToken, nil
+}
+
+func (s *service) Create(request access_token.AccessTokenRequest) (*access_token.AccessToken, *errors.RestErr) {
+	if err := request.Validate(); err != nil {
+		return nil, err
+	}
+	user, err := s.restUsersRepo.LoginUser(request.Username, request.Password)
+	if err != nil {
+		return nil, err
+	}
+	at := access_token.GetNewAccessToken(user.Id)
+	at.Generate()
+
+	// Save the new access token in Cassandra:
+	if err := s.dbRepo.Create(at); err != nil {
+		return nil, err
+	}
+	return &at, nil
+}
+
+func (s *service) UpdateExpirationTime(at access_token.AccessToken) *errors.RestErr {
+	if err := at.Validate(); err != nil {
+		return err
+	}
+	return s.dbRepo.UpdateExpirationTime(at)
+}
